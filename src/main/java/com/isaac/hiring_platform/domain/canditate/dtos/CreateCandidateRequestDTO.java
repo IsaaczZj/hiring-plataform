19a66dba2fd9483a7ee5c388097dff604132a644
isaac_dev <joseisaac.isaac123@gmail.com>
2026-07-09 17:04:03 -0300

    feat: criando validacoes do dto de candidato

--- src/main/java/com/isaac/hiring_platform/domain/canditate/dtos/CreateCandidateRequestDTO.java
+++ src/main/java/com/isaac/hiring_platform/domain/canditate/dtos/CreateCandidateRequestDTO.java
@@ -1,6 +1,29 @@
 package com.isaac.hiring_platform.domain.canditate.dtos;
 
+import jakarta.validation.constraints.*;
+
+
 public record CreateCandidateRequestDTO(
 
+        @NotBlank(message = "Digite um nome")
+        @Size(min = 2, message = "O nome precisa ter no mínimo 2 caracteres")
+        String name,
+
+        @NotBlank(message = "Digite um username")
+        @Size(min = 2, message = "O username precisa ter no mínimo 2 caracteres")
+        String username,
+
+        @Email(message = "Digite um e-mail válido")
+        String email,
+
+        @NotBlank(message = "Digite uma senha")
+        @Size(min = 4, message = "O senha precisa ter no mínimo 4 caracteres")
+        String password,
+        @NotBlank(message = "Digite uma descrição")
+        @Size(min = 10, max = 255, message = "A descrição precisa ter entre 10 e 255 caracteres")
+        String description,
+
+        @NotBlank(message = "Coloque um currículo")
+        String curriculum
 ) {
 }
