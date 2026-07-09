9c95ee4c8f794ccf4d8519ac2748654e2b6bc60e
isaac_dev <joseisaac.isaac123@gmail.com>
2026-07-09 17:18:56 -0300

    feat: criando compose do banco de dados e variaveis de ambiente

--- src/main/java/com/isaac/hiring_platform/domain/canditate/CandidateController.java
+++ src/main/java/com/isaac/hiring_platform/domain/canditate/CandidateController.java
@@ -1,13 +1,21 @@
 package com.isaac.hiring_platform.domain.canditate;
 
+import com.isaac.hiring_platform.domain.canditate.dtos.CreateCandidateRequestDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/candidate")
+@Validated
 public class CandidateController {
 
     @PostMapping
-    public void create(){}
+    public void create(@Valid @RequestBody CreateCandidateRequestDTO body) {
+        System.out.println(body);
+    }
 }
