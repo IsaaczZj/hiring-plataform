1a746809bc3ecb250a95d22fc01b170da089e743
isaac_dev <joseisaac.isaac123@gmail.com>
2026-07-09 16:19:56 -0300

    initial commit

---
+++ src/main/java/com/isaac/hiring_platform/HiringPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.isaac.hiring_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiringPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiringPlatformApplication.class, args);
+	}
+
+}
